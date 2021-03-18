0748a022a63ca1fb125fe4ba6f6271895363e6d7
kjihyeon <jihyun2054@naver.com>
2021-03-18 23:22:59 +0900

    sign in - 1

--- app/src/main/java/com/jm5/appus/dataForm/JWT.kt
+++ app/src/main/java/com/jm5/appus/dataForm/JWT.kt
@@ -3,9 +3,5 @@ package com.jm5.appus.dataForm
 import java.io.Serializable
 
 data class JWT (
-<<<<<<< HEAD
+
         var jwt: String):Serializable
-=======
-        var jwt : String
-):Serializable
->>>>>>> origin/main
